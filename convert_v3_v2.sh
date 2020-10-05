60088263aab2d22c9367364a7f2a9765596dc3b5
Markus Michels <markus7017@gmail.com>
2020-10-05 15:38:02 +0200

    updated

---
+++ convert_v3_v2.sh
@@ -0,0 +1,10 @@
+find . -name *.java -exec sed -i '' -e "s/org.openhab.core.thing./org.eclipse.smarthome.core.thing./g" {} \;
+find . -name *.java -exec sed -i '' -e "s/org.openhab.core.i18n./org.eclipse.smarthome.core.i18n./g" {} \;
+find . -name *.java -exec sed -i '' -e "s/org.openhab.core.library.unit./org.eclipse.smarthome.core.library.unit./g" {} \;
+find . -name *.java -exec sed -i '' -e "s/org.openhab.core.library.types./org.eclipse.smarthome.core.library.types./g" {} \;
+find . -name *.java -exec sed -i '' -e "s/org.openhab.core.types./org.eclipse.smarthome.core.types./g" {} \;
+find . -name *.java -exec sed -i '' -e "s/org.openhab.core.net./org.eclipse.smarthome.core.net./g" {} \;
+find . -name *.java -exec sed -i '' -e "s/org.openhab.core.config.discovery./org.eclipse.smarthome.config.discovery./g" {} \;
+find . -name *.java -exec sed -i '' -e "s/org.openhab.core.io.net.http.HttpClientFactory/org.eclipse.smarthome.io.net.http.HttpClientFactory/g" {} \;
+mvn spotless:apply
+
