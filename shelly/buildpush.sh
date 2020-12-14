0c58997d0f66483865a933c3bc033920ff7c6ecc
Markus Michels <markus7017@gmail.com>
2020-12-14 08:55:10 +0100

    updated

--- shelly/buildpush.sh
+++ shelly/buildpush.sh
@@ -12,7 +12,7 @@ then
 	echo Build version 2 failed
 	exit
 fi
-cp target/org.openhab.binding.shelly-2.5.11-SNAPSHOT.jar ~/Dev/myfiles/shelly/
+cp target/org.openhab.binding.shelly-2.5.*-SNAPSHOT.jar ~/Dev/myfiles/shelly/
 
 echo Copy code to V3, update Shelly from v2 to v3
 cd ~/Dev/openhab-3/git/openhab-addons/bundles/org.openhab.binding.shelly
@@ -31,7 +31,7 @@ then
 	echo Build version 3 failed
 	exit
 fi
-cp target/org.openhab.binding.shelly-3.0.0-SNAPSHOT.jar ~/Dev/myfiles/shelly/
+cp target/org.openhab.binding.shelly-3.0.*-SNAPSHOT.jar ~/Dev/myfiles/shelly/
 
 echo Pushing updates
 cd ~/Dev/myfiles
