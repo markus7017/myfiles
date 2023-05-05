7cf451dec983b671c7ca7aad09a57703c4a4fcd0
Markus Michels <markus7017@gmail.com>
2023-05-06 00:59:00 +0200

    updated

--- convert_v4_v3.sh
+++ convert_v4_v3.sh
@@ -1,2 +1,3 @@
 find . -name *.java -exec sed -i '' -e "s/2010-2023/2010-2022/g" {} \;
+LC_CTYPE=C find . -name "*.properties" -exec sed -i '' -e 's/addon\.shelly/binding\.shelly/g' {} \;
 mvn spotless:apply
