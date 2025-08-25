8751656c69f02da2567d42bf9ba7b017a59e4774
Markus Michels <markus7017@gmail.com>
2025-08-25 17:49:22 +0200

    updated

--- shelly/convert_v5_v4.sh
+++ shelly/convert_v5_v4.sh
@@ -1,5 +1,4 @@
 #!/bin/bash
-cp -r ~/Dev/openhab-5/git/openhab-addons/bundles/org.openhab.binding.shelly/src/* src/
 find . -name "*.java" -exec sed -i '' -e ':a' -e 'N' -e '$!ba' -e 's|/\*\n \* Copyright (c) 2010-2025|/\*\*\n \* Copyright (c) 2010-2024|g' {} +
 find . -name "*.java" -exec sed -i '' -e ':a' -e 'N' -e '$!ba' -e 's|import org.eclipse.californium.elements.config.Configuration;\nimport org.eclipse.californium.scandium.config.DtlsConfig;|import org.eclipse.californium.core.network.config.NetworkConfig;|g' {} +
 find . -name "*.java" -exec sed -i '' -e ':a' -e 'N' -e '$!ba' -e 's|import org.eclipse.californium.elements.config.Configuration;\n||g' {} +
@@ -8,3 +7,5 @@ find . -name "*.java" -exec sed -i '' -e ':a' -e 'N' -e '$!ba' -e 's|    static 
 find . -name "*.xml" -exec  sed -i '' -E 's|<semantic-equipment-tag>.*<\/semantic-equipment-tag>||g'  {} \;
 cp ~/Dev/myfiles/shelly/Shelly1CoapServer.java src/main/java/org/openhab/binding/shelly/internal/api1/
 mvn spotless:apply
+mvn clean compile
+
