afbc9c7ec2b6430d37b4d50f64179d09810e3497
Markus Michels <markus7017@gmail.com>
2025-09-03 13:33:21 +0200

    updated

--- cp50.sh
+++ cp50.sh
@@ -2,7 +2,11 @@
 OLDPWD=$(pwd)
 
 ohdir=~/Dev/openhab-5.0.1/git/openhab-addons/bundles/$ohbundledir
-destdir=~/Dev/myfiles/$ohbundle/
+destdir=~/Dev/myfiles/$ohbundle
+suffix=""
+if [[ -n "$1" ]]; then
+  suffix="-$1"
+fi
 
 echo Copy Code von $ohdir5/$ohbundledir/src/
 cd $ohdir
@@ -10,9 +14,12 @@ rm -rf ./src
 rm -rf ./target
 if cp -r $ohdir5/$ohbundledir/src/ src/; then
 	find . -name ".DS_Store" -type f -delete
-	if mvn clean install; then
-	  cp target/org.openhab.binding*.jar $destdir
-	  rm -f $destdir/*sources.jar
+	if mvn clean install -D skipChecks; then
+      jar=$(ls target/org.openhab.binding.${ohbundle}-*-SNAPSHOT.jar 2>/dev/null | head -n 1)
+      basename="${jar%.jar}"
+      echo "cp $basename.jar $destdir/$basename$suffix.jar"
+      read
+	  cp $basename.jar $destdir/$basename$suffix.jar
 	  echo "Build successful, jar copied to $destdir"
 	  cd ~/Dev/myfiles
 	  ./push.sh
