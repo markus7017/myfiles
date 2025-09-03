0337473264eb87580d507272e55a5691b763dba1
Markus Michels <markus7017@gmail.com>
2025-09-03 13:58:29 +0200

    updated

--- cp4.sh
+++ cp4.sh
@@ -3,6 +3,10 @@ OLDPWD=$(pwd)
 
 ohdir=$ohdir4/$ohbundledir
 destdir=~/Dev/myfiles/$ohbundle/
+suffix=""
+if [[ -n "$1" ]]; then
+  suffix="-$1"
+fi
 
 cd $ohdir
 echo Copy Code von $ohdir5/$ohbundledir/src/ to $(pwd)/src
@@ -13,9 +17,13 @@ if cp -r $ohdir5/$ohbundledir/src/ src/; then
     ~/Dev/myfiles/$ohbundle/convert_v5_v4.sh
 	find . -name ".DS_Store" -type f -delete
 	if mvn clean install -Pj17 -Dohc.version=4.3.0; then
-	  cp target/org.openhab.binding*.jar $destdir
-	  rm -f $destdir/*sources.jar
-	  echo "Build successful, jar copied to $destdir"
+      jar=$(ls target/org.openhab.binding.${ohbundle}-*-SNAPSHOT.jar 2>/dev/null | head -n 1)
+      basename="${jar#target/}"
+      basename="${basename%.jar}"
+      src_name="target/$basename.jar"
+      dst_name="$destdir/$basename$suffix.jar"
+	  cp $src_name $dst_name
+	  echo "Build successful, $jar copied to $destdir"
 	  cd ~/Dev/myfiles
 	  ./push.sh
     fi
--- cp51.sh
+++ cp51.sh
@@ -11,11 +11,15 @@ fi
 cd $ohdir
 find . -name ".DS_Store" -type f -delete
 if mvn clean install; then
-  cp target/org.openhab.binding*.jar $destdir
-  rm -f $destdir/*sources.jar
-  echo "Build successful, jar copied to $destdir"
-  cd ~/Dev/myfiles
-  ./push.sh
+      jar=$(ls target/org.openhab.binding.${ohbundle}-*-SNAPSHOT.jar 2>/dev/null | head -n 1)
+      basename="${jar#target/}"
+      basename="${basename%.jar}"
+      src_name="target/$basename.jar"
+      dst_name="$destdir/$basename$suffix.jar"
+	  cp $src_name $dst_name
+	  echo "Build successful, $jar copied to $destdir"
+	  cd ~/Dev/myfiles
+	  ./push.sh
 else
   echo "Build failed"
 fi
